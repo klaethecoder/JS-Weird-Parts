8c88f104e832bc4dc5dcbfb7bbd65c44fc71e26b
Ryan Richard <ryanrichard@Ryans-MacBook-Pro.local>
2020-06-19 22:44:16 -0600

    Learned the Execution stack and how it works with functions and variables. Created an example that helps explain the order of how things get put on and taken off of the Stack

--- starter.js
+++ starter.js
@@ -32,6 +32,38 @@
     empty();
     console.log(change);
 
+    let a =1;
+
     // Creation phase is where the Global Variables and Objects and functions are set aside to memory first and in the Execution phase is where the variables get added to the Engines memory
 
-    
+    // Using guard statements to do an if/else statement to see how JS uses undefinted
+    if (a === undefined) console.log("a is undefined")
+    else console.log('a is defined')
+
+    // After the Creation phase and the hoisting, it goes through the execution phase to run my code. 
+
+    // Single Threaded: One command is being executed at one time. 
+
+    // Syncrononus: One at a time in a specific order. 
+
+    // Function Invocation and Execution Stack
+        // Function Invocation: Running of the JS Funtion i.e.()
+        
+        // Below is an example of how the execution stack works
+        function first(){
+            console.log("I'm First!")
+        }
+
+        function second(){
+        first();
+            return console.log("Second Place")
+            console.log("Never run")
+        }
+
+        function third(){
+            second();
+            console.log("Last statement")
+        }
+
+        third();
+        
